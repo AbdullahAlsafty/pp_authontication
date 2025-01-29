0ca3151b36c0882e4424affb1329fe802c1a420f
AbdullahAlsafty <abdullahmohammedmoawad.com>
2025-01-30 00:08:12 +0200

    php file auth done

---
+++ auth/signUp.php
@@ -0,0 +1,21 @@
+<?php
+include '../functions/filter_request.php';
+include '../connect.php';
+echo 'kkkkkkkkkkkk';
+
+
+$userName = filterRequest('userName');
+$password = filterRequest('password');
+$email = filterRequest('email');
+
+$std = $con->prepare(
+    "INSERT INTO `users` ( `userName`, `email`, `password`)VALUES ( ?, ?, ?)");
+    $std->execute(array($userName,$email,$password));
+    
+    
+    $countRow = $std->rowCount();
+    if($countRow>0){
+        echo json_encode(array('Status'=>'Success','Row Coun'=>$countRow));
+    
+    }else {
+        echo json_encode(array('Status'=>'Failure','Row Coun'=>$countRow));}
--- index.php
+++ index.php
@@ -1,15 +1,2 @@
 <?php
-include "authentication/signUp.php";
-
-$std = $con->prepare(
-"INSERT INTO `users` ( `userName`, `email`, `password`)VALUES ( ?, ?, ?)");
-$std->execute(array($userName,$email,$password));
-
-
-$countRow = $std->rowCount();
-if($countRow>0){
-    echo 'Success >> '. $countRow;
-
-}else {
-    echo "Failur >>". $countRow;
-}
+ echo '====== === index index ';
